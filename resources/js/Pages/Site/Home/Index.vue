2ec70c968e6936a3862e02e9a04ce662816fe74b
Heitor <heitor@agily.com.br>
2023-07-03 20:20:30 -0300

    continuação home

--- resources/js/Pages/Site/Home/Index.vue
+++ resources/js/Pages/Site/Home/Index.vue
@@ -4,6 +4,7 @@
     import { ref } from 'vue';
 
     const slide = ref(1);
+    const slide2 = ref(1);
     const autoplay = ref(false);
     const controlType = ref('push');
 
@@ -30,7 +31,7 @@
                     height="500px"
                     control-color="blue-grey-10"
                     :control-type="controlType"
-                    class="bg-grey-9 shadow-2 no-padding"
+                    class="bg-grey-9"
                 >
                     <q-carousel-slide :name="1" class="no-padding">
                         <!-- <div class="row fit justify-center"> -->
@@ -57,10 +58,88 @@
         </div>
 
         <div class="row">
-            <div class="col-12 bg-blue-grey-10" style="height: 500px;">
+            <div class="col-6 bg-dark">
+                <div class="flex flex-center bg-danger text-white app-fs-30 app-fw-650 q-pt-lg">
+                    Serviços
+                </div>
+                <q-carousel
+                    animated
+                    swipeable
+                    v-model="slide2"
+                    infinite
+                    :autoplay="autoplay"
+                    transition-prev="slide-right"
+                    transition-next="slide-left"
+                    @mouseenter="autoplay = false"
+                    @mouseleave="autoplay = true"
+                    height="550px"
+                    class="bg-dark q-py-xl q-px-md"
+                    ref="carousel"
+                >
+                    <q-carousel-slide :name="1" class="column no-wrap no-padding ">
+                        <div class="row fit justify-center items-center q-gutter-sm q-col-gutter no-wrap">
+                            <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/mountains.jpg" />
+                            <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/parallax1.jpg" />
+                        </div>
+                    </q-carousel-slide>   
+                    
+                    <q-carousel-slide :name="2" class="column no-wrap no-padding">
+                        <div class="row fit justify-center items-center q-gutter-sm q-col-gutter no-wrap">
+                            <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+                            <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/quasar.jpg" />
+                        </div>
+                    </q-carousel-slide>
 
+                    <q-carousel-slide :name="3" class="column no-wrap no-padding">
+                        <div class="row fit justify-center items-center q-gutter-sm q-col-gutter no-wrap">
+                            <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/cat.jpg" />
+                            <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/linux-avatar.png" />
+                        </div>
+                    </q-carousel-slide>
+
+                    <q-carousel-slide :name="4" class="column no-wrap no-padding">
+                        <div class="row fit justify-center items-center q-gutter-sm q-col-gutter no-wrap">
+                            <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/material.png" />
+                            <q-img class="rounded-borders col-6 full-height" src="https://cdn.quasar.dev/img/donuts.png" />
+                        </div>
+                    </q-carousel-slide>
+
+                    <template v-slot:control>
+
+                        <q-carousel-control
+                            position="bottom-left"
+                            :offset="[25, 60]"
+                            class="q-gutter-xs"
+                        >
+                        <q-btn
+                            push round dense color="blue-grey" text-color="white" icon="arrow_left"
+                            @click="$refs.carousel.previous()"
+                        />
+                        <q-btn
+                            push round dense color="blue-grey" text-color="white" icon="arrow_right"
+                            @click="$refs.carousel.next()"
+                        />
+                        </q-carousel-control>
+                    </template>            
+                </q-carousel>
+
+            </div>       
+
+
+            <div class="col-6 bg-dark">
+                <div class="flex flex-center">
+
+                </div>
+                <div class="flex flex-center text-white app-fs-30 app-fw-650 q-pt-lg">
+                    Assistências Autorizadas
+                </div>
             </div>
         </div>
 
+        <div class="row">
+            <div class="col-12 bg-red-14" style="height: 500px;">
+
+            </div>
+        </div>
     </MainLayout>
 </template>
