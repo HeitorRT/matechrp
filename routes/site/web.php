2f804aabf0ad7915459d61ee645785b28bba7b39
Heitor <heitor@agily.com.br>
2023-09-27 15:30:16 -0300

    ajuste rotas

--- routes/site/web.php
+++ routes/site/web.php
@@ -8,10 +8,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/home');
 
-Route::group(function() {
-    Route::get('/home', [HomeController::class, 'index'])->name('site.home');
-    Route::get('/soon', [HomeController::class, 'soon'])->name('site.soon');
-    Route::get('/about-us', [AboutUsController::class, 'index'])->name('site.about-us');
-    Route::get('/contact', [ContactController::class, 'index'])->name('site.contact');
-    Route::get('/services', [ServiceController::class, 'index'])->name('site.services');
+Route::name('site.')->group(function() {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/soon', [HomeController::class, 'soon'])->name('soon');
+    Route::get('/about-us', [AboutUsController::class, 'index'])->name('about-us');
+    Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+    Route::get('/services', [ServiceController::class, 'index'])->name('services');
+    Route::get('/services/{service}', [ServiceController::class, 'show'])->name('services.show');
 });
