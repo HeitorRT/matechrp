3a048213c51ec5b1e68f23623d3b1b9c2a139b5b
Heitor <heitor@agily.com.br>
2023-12-01 19:56:42 -0300

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,10 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // if ($this->app->environment('production')) {
-        //     $this->app['request']->server->set('HTTPS','on');
-        //     URL::forceScheme('https');
-        // }
+        if ($this->app->environment('production')) {
+            $this->app['request']->server->set('HTTPS','on');
+            URL::forceScheme('https');
+        }
 
         JsonResource::withoutWrapping();
     }
